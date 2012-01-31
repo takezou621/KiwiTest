c9dab524716d4e520bf9046e9381bc9d7b66ed3a
Takeshi Kawai <takeshi.kawai@gree.co.jp>
2012-01-31 15:27:16 +0900

    Initial Commit

---
+++ KiwiTestProj/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KiwiTestProj
+//
+//  Created by takeshi.kawai on 12/01/31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KiwiTestProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  KiwiTestProj
+//
+//  Created by takeshi.kawai on 12/01/31.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
